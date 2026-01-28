778bb4432ae71b6025cb5a59b1cc2639469475ef
Evgeniy-makdak <makdak23@mail.ru>
2026-01-28 15:11:05 +0300

    Реализация слайдера со стрелками

--- slider.js
+++ slider.js
@@ -1,14 +1,30 @@
 const texts = ["text1", "text2", "text3"];
 const textOnDiv = document.getElementById("slider");
-const button = document.getElementById("btn");
+const arrowLeft = document.querySelector("#left");
+const arrowRight = document.querySelector("#right");
+let i = 0;
 
-button.addEventListener("click", function () {
-  let i = 0;
-  setInterval(() => {
-    textOnDiv.textContent = texts[i];
-    i++;
-    if (i === texts.length) {
-      i = 0;
-    }
-  }, 1000);
+function renderText() {
+  textOnDiv.textContent = texts[i];
+}
+
+arrowLeft.addEventListener("click", function () {
+  i--;
+  if (i === -1) {
+    i--;
+    i = 2;
+    renderText();
+  } else {
+    renderText();
+  }
+});
+
+arrowRight.addEventListener("click", function () {
+  i++;
+  if (i === 3) {
+    i = 0;
+    renderText();
+  } else {
+    renderText();
+  }
 });
