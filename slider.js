89292bc6c1118a0935b506dbb07f85d8162f06b7
Evgeniy-makdak <makdak23@mail.ru>
2026-01-30 21:57:14 +0300

    Исправил деактивацию кнопок

--- slider.js
+++ slider.js
@@ -3,28 +3,29 @@ const textOnDiv = document.getElementById("slider");
 const arrowLeft = document.querySelector("#left");
 const arrowRight = document.querySelector("#right");
 let i = 0;
+textOnDiv.textContent = texts[0];
 
 function renderText() {
   textOnDiv.textContent = texts[i];
 }
 
 arrowLeft.addEventListener("click", function () {
-  i--;
-  if (i === -1) {
+  if (i === 0) {
     arrowLeft.disabled = true;
     return;
   } else {
+    --i;
     renderText();
     arrowRight.disabled = false;
   }
 });
 
 arrowRight.addEventListener("click", function () {
-  i++;
-  if (i === 3) {
+  if (i === 2) {
     arrowRight.disabled = true;
     return;
   } else {
+    ++i;
     renderText();
     arrowLeft.disabled = false;
   }
